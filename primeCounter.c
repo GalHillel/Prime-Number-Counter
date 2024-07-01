2b625668a6d563a59073beb82ffec9e591fbd3ce
Gal Hillel <Galh2011@gmail.com>
2024-07-01 10:33:12 +0300

    Update primeCounter.c

--- primeCounter.c
+++ primeCounter.c
@@ -1,73 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
+#include <stdint.h>
 #include <unistd.h>
-#include <sys/resource.h>
+#include <time.h>
 
 #define NUM_THREADS 8
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 4096
+#define RAM_LIMIT (2 * 1024 * 1024) // 2MB
 
-typedef struct
-{
-    long long buffer[BUFFER_SIZE];
-    int start;
-    int end;
-    int count;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond_nonempty;
-    pthread_cond_t cond_nonfull;
-} buffer_t;
+pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t buffer_not_empty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t buffer_not_full = PTHREAD_COND_INITIALIZER;
 
-buffer_t buffer = {
-    .start = 0,
-    .end = 0,
-    .count = 0,
-    .mutex = PTHREAD_MUTEX_INITIALIZER,
-    .cond_nonempty = PTHREAD_COND_INITIALIZER,
-    .cond_nonfull = PTHREAD_COND_INITIALIZER};
-
-volatile int total_primes = 0;
-
-long long mulmod(long long a, long long b, long long mod)
-{
-    long long x = 0, y = a % mod;
-    while (b > 0)
-    {
-        if (b % 2 == 1)
-        {
-            x = (x + y) % mod;
-        }
-        y = (y * 2) % mod;
-        b /= 2;
-    }
-    return x % mod;
-}
+int buffer[BUFFER_SIZE];
+int buffer_count = 0;
+int total_counter = 0;
+bool done = false;
 
 long long modulo(long long base, long long exponent, long long mod)
 {
-    long long x = 1;
-    long long y = base;
-    while (exponent > 0)
+    long long result = 1;
+    base %= mod;
+    while (exponent)
     {
         if (exponent % 2 == 1)
-        {
-            x = (x * y) % mod;
-        }
-        y = (y * y) % mod;
-        exponent = exponent / 2;
+            result = (result * base) % mod;
+        base = (base * base) % mod;
+        exponent /= 2;
     }
-    return x % mod;
+    return result;
 }
 
-int Miller(long long p, int iteration)
+long long mulmod(long long a, long long b, long long mod)
+{
+    long long result = 0;
+    a %= mod;
+    while (b)
+    {
+        if (b % 2 == 1)
+            result = (result + a) % mod;
+        a = (a * 2) % mod;
+        b /= 2;
+    }
+    return result;
+}
+
+bool is_small_prime(long long p)
+{
+    const int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    for (int i = 0; i < sizeof(primes) / sizeof(primes[0]); i++)
+    {
+        if (p == primes[i])
+            return true;
+        if (p % primes[i] == 0)
+            return false;
+    }
+    return true;
+}
+
+bool MillerRabin(long long p, int iteration)
 {
     if (p < 2)
     {
-        return 0;
+        return false;
+    }
+    if (is_small_prime(p))
+    {
+        return true;
     }
     if (p != 2 && p % 2 == 0)
     {
-        return 0;
+        return false;
     }
     long long s = p - 1;
     while (s % 2 == 0)
@@ -85,89 +90,74 @@ int Miller(long long p, int iteration)
         }
         if (mod != p - 1 && temp % 2 == 0)
         {
-            return 0;
+            return false;
         }
     }
-    return 1;
+    return true;
 }
 
-void *worker(void *arg)
+void *prime_counter_thread(void *arg)
 {
+    int iteration = 5; // Number of iterations for Miller-Rabin
     while (1)
     {
-        pthread_mutex_lock(&buffer.mutex);
-        while (buffer.count == 0)
+        pthread_mutex_lock(&buffer_mutex);
+        while (buffer_count == 0 && !done)
         {
-            pthread_cond_wait(&buffer.cond_nonempty, &buffer.mutex);
+            pthread_cond_wait(&buffer_not_empty, &buffer_mutex);
         }
-        long long num = buffer.buffer[buffer.start];
-        buffer.start = (buffer.start + 1) % BUFFER_SIZE;
-        buffer.count--;
-        pthread_cond_signal(&buffer.cond_nonfull);
-        pthread_mutex_unlock(&buffer.mutex);
-
-        if (num == -1)
-            break;
-
-        if (Miller(num, 5))
+        if (buffer_count == 0 && done)
         {
-            __sync_fetch_and_add(&total_primes, 1);
+            pthread_mutex_unlock(&buffer_mutex);
+            break;
+        }
+
+        int num = buffer[--buffer_count];
+        pthread_cond_signal(&buffer_not_full);
+        pthread_mutex_unlock(&buffer_mutex);
+
+        if (MillerRabin(num, iteration))
+        {
+            __sync_fetch_and_add(&total_counter, 1);
         }
     }
     return NULL;
 }
 
-void print_memory_usage()
-{
-    struct rusage usage;
-    getrusage(RUSAGE_SELF, &usage);
-    printf("Memory usage: %ld KB\n", usage.ru_maxrss);
-}
-
 int main()
 {
     pthread_t threads[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; i++)
+
+    for (int i = 0; i < NUM_THREADS; ++i)
     {
-        pthread_create(&threads[i], NULL, worker, NULL);
+        pthread_create(&threads[i], NULL, prime_counter_thread, NULL);
     }
 
-    long long num;
-    while (scanf("%lld", &num) != EOF)
+    int num;
+    while (scanf("%d", &num) != EOF)
     {
-        pthread_mutex_lock(&buffer.mutex);
-        while (buffer.count == BUFFER_SIZE)
+        pthread_mutex_lock(&buffer_mutex);
+        while (buffer_count == BUFFER_SIZE)
         {
-            pthread_cond_wait(&buffer.cond_nonfull, &buffer.mutex);
+            pthread_cond_wait(&buffer_not_full, &buffer_mutex);
         }
-        buffer.buffer[buffer.end] = num;
-        buffer.end = (buffer.end + 1) % BUFFER_SIZE;
-        buffer.count++;
-        pthread_cond_signal(&buffer.cond_nonempty);
-        pthread_mutex_unlock(&buffer.mutex);
+
+        buffer[buffer_count++] = num;
+        pthread_cond_signal(&buffer_not_empty);
+        pthread_mutex_unlock(&buffer_mutex);
     }
 
-    for (int i = 0; i < NUM_THREADS; i++)
-    {
-        pthread_mutex_lock(&buffer.mutex);
-        while (buffer.count == BUFFER_SIZE)
-        {
-            pthread_cond_wait(&buffer.cond_nonfull, &buffer.mutex);
-        }
-        buffer.buffer[buffer.end] = -1;
-        buffer.end = (buffer.end + 1) % BUFFER_SIZE;
-        buffer.count++;
-        pthread_cond_signal(&buffer.cond_nonempty);
-        pthread_mutex_unlock(&buffer.mutex);
-    }
+    pthread_mutex_lock(&buffer_mutex);
+    done = true;
+    pthread_cond_broadcast(&buffer_not_empty);
+    pthread_mutex_unlock(&buffer_mutex);
 
-    for (int i = 0; i < NUM_THREADS; i++)
+    for (int i = 0; i < NUM_THREADS; ++i)
     {
         pthread_join(threads[i], NULL);
     }
 
-    printf("%d total primes.\n", total_primes);
-    print_memory_usage();
+    printf("%d total primes.\n", total_counter);
 
     return 0;
 }
