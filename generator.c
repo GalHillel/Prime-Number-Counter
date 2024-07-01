799c7f6b0950d2ea45f35e07cfd7e8dadd223110
Gal Hillel <Galh2011@gmail.com>
2024-07-01 10:33:06 +0300

    Update generator.c

--- generator.c
+++ generator.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         fprintf(stderr, "Usage: %s <seed> <count>\n", argv[0]);
         return 1;
     }
@@ -16,16 +15,23 @@ int main(int argc, char *argv[])
 
     // Set seed for random number generator
     srand(seed);
-
+    
     int lowerLimit = 1000000;
     int upperLimit = 2100000000;
 
+    // Start measuring time
+    clock_t start_time = clock();
+
     // Generate and output random numbers
-    for (int i = 0; i < count; ++i)
-    {
+    for (int i = 0; i < count; ++i) {
         int random_number = rand() % (upperLimit - lowerLimit + 1) + lowerLimit;
         printf("%d\n", random_number);
+        fflush(stdout);
     }
+
+    clock_t end_time = clock();
+    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    // printf("Execution time : %.6f seconds\n", elapsed_time);
 
     return 0;
 }
